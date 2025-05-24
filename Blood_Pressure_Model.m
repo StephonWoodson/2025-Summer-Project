27a23f795c656c3afefa691806ef15bdac553f78
StephonWoodson <woods5sm@dukes.jmu.edu>
2025-05-23 23:27:35 -0400

    Add files via upload
    Files of the blood pressure modeling. So far, there is only cardiac output.

---
+++ Blood_Pressure_Model.m
@@ -0,0 +1,11 @@
+%% Cardiac Output Variables
+
+Ca = 87.5; %The oxygen content arterial blood
+Co = 80; %The oxygen content of venous blood
+VO2 = 37.5; % The maximum oxygen consumption
+
+% Equation VO2/(Ca - Co) = CO
+
+% We want Cardiac output to be around 5-6
+% Below 2.25 is too low of a cardiac output
+% Above 8 is too high of a cardiac output
