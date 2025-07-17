aee9e0d4a88c2ebf1cfe32edc0d8adad56ebc921
Stephon <stephonwoodson122@gmail.com>
2025-07-16 23:26:39 -0400

    updating my work

---
+++ Bernoulli's/Boullis_Equation.m
@@ -0,0 +1,11 @@
+%% Boulli's equation
+p = 1.060; %blood density__kg/m^3
+V2 = 38; % velocity at point two__m/s
+P1 = 725; %Pressure at point one__Pa
+
+% Pa = kg/(m x s^2)
+% Pa = 1N/m^2
+% How the math works --- x/9.8066135801985
+
+V1 = 0; %Velocity at point one__cm/s
+
